cb17a8f57b89f6a28a12382ad5c632ca561bf23a
magmamaybecodes <binay.adhikari2@gmail.com>
2026-07-19 21:13:15 +0545

    from text to icons

--- src/features/learning/GuidedMode.cpp
+++ src/features/learning/GuidedMode.cpp
@@ -31,7 +31,110 @@ static void OpenWorkspaceWithObject(int objectType)
     SetGuidedWorkspace(true);
     sceneManagerChangeScene(learnSceneId::LEARN_FREEDRAW);
 }
+enum GuidedButtonIcon
+{
+    ICON_CUBE,
+    ICON_SPHERE,
+    ICON_CYLINDER,
+    ICON_IMPORT
+};
 
+static void DrawCubeIcon(Rectangle r)
+{
+    Color c = Fade(WHITE, 0.22f);
+
+    float size = r.width * 0.32f;
+
+    Vector2 f1 = { r.x + r.width * 0.33f, r.y + r.height * 0.26f };
+    Vector2 f2 = { f1.x + size, f1.y };
+    Vector2 f3 = { f2.x, f2.y + size };
+    Vector2 f4 = { f1.x, f1.y + size };
+
+    Vector2 off = { size * 0.25f, -size * 0.25f };
+
+    Vector2 b1 = Vector2Add(f1, off);
+    Vector2 b2 = Vector2Add(f2, off);
+    Vector2 b3 = Vector2Add(f3, off);
+    Vector2 b4 = Vector2Add(f4, off);
+
+    const float t = 2.0f;
+
+    DrawLineEx(f1, f2, t, c);
+    DrawLineEx(f2, f3, t, c);
+    DrawLineEx(f3, f4, t, c);
+    DrawLineEx(f4, f1, t, c);
+
+    DrawLineEx(b1, b2, t, c);
+    DrawLineEx(b2, b3, t, c);
+    DrawLineEx(b3, b4, t, c);
+    DrawLineEx(b4, b1, t, c);
+
+    DrawLineEx(f1, b1, t, c);
+    DrawLineEx(f2, b2, t, c);
+    DrawLineEx(f3, b3, t, c);
+    DrawLineEx(f4, b4, t, c);
+}
+
+static void DrawSphereIcon(Rectangle r)
+{
+    Color c = Fade(WHITE, 0.22f);
+
+    Vector2 center =
+    {
+        r.x + r.width / 2,
+        r.y + r.height * 0.40f
+    };
+
+    float radius = r.width * 0.19f;
+
+    DrawCircleLinesV(center, radius, c);
+
+    DrawEllipseLines(
+        (int)center.x,
+        (int)center.y,
+        radius * 0.45f,
+        radius,
+        c);
+
+    DrawEllipseLines(
+        (int)center.x,
+        (int)center.y,
+        radius,
+        radius * 0.35f,
+        c);
+}
+
+static void DrawCylinderIcon(Rectangle r)
+{
+    Color c = Fade(WHITE, 0.22f);
+
+    float w = r.width * 0.34f;
+    float h = r.height * 0.32f;
+
+    float x = r.x + r.width / 2;
+    float y = r.y + r.height * 0.22f;
+
+    DrawEllipseLines((int)x, (int)y, w / 2, 8, c);
+
+    DrawLineEx({ x - w / 2, y }, { x - w / 2, y + h }, 2, c);
+    DrawLineEx({ x + w / 2, y }, { x + w / 2, y + h }, 2, c);
+
+    DrawEllipseLines((int)x, (int)(y + h), w / 2, 8, c);
+}
+
+static void DrawImportIcon(Rectangle r)
+{
+    Color c = Fade(WHITE, 0.22f);
+
+    float cx = r.x + r.width / 2;
+    float cy = r.y + r.height * 0.36f;
+
+    DrawLineEx({ cx, cy + 18 }, { cx, cy - 10 }, 3, c);
+    DrawLineEx({ cx, cy - 10 }, { cx - 10, cy }, 3, c);
+    DrawLineEx({ cx, cy - 10 }, { cx + 10, cy }, 3, c);
+
+    DrawLineEx({ cx - 20, cy + 22 }, { cx + 20, cy + 22 }, 3, c);
+}
 //---------------------------------------------------------
 // Draw a custom rounded button
 //---------------------------------------------------------
--- src/features/learning/GuidedMode.h
+++ src/features/learning/GuidedMode.h
@@ -1,8 +1,21 @@
 #pragma once
 
+#include "raylib.h"
+
+enum GuidedButtonIcon
+{
+    ICON_CUBE,
+    ICON_SPHERE,
+    ICON_CYLINDER,
+    ICON_IMPORT
+};
+
 void GuidedModeInit();
 void GuidedModeUpdate();
 void GuidedModeDraw();
 void GuidedModeUnload();
 
-void DrawRoundedButton(Rectangle bounds, Color color, const char* text);
+void DrawRoundedButton(Rectangle bounds,
+                       Color color,
+                       const char* text,
+                       GuidedButtonIcon icon);
